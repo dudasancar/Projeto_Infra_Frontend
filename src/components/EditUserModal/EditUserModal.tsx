6a24e0107d70615501ef0e3c5cc3468135a1a8bd
Mateus Lammel <lammel.mateus@gmail.com>
2022-04-11 15:21:31 -0300

    Estilização completa do modal

--- src/components/EditUserModal/EditUserModal.tsx
+++ src/components/EditUserModal/EditUserModal.tsx
@@ -1,22 +1,100 @@
 import React from 'react'
-import { Container, ModalForm } from './Style'
-import {Button, TextField} from '@mui/material'
+import { Container, ModalForm, ContainerButtons, GridForm } from './Style'
+import {Button, MenuItem, TextField} from '@mui/material'
+import {useFormik} from 'formik';
+
 
 
 const EditUserModal = () => {
+
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      name:'',
+      email: '',
+      userType: '',
+    },
+    onSubmit: (values: {name: string, email: string, userType: string} ) => {
+
+    },
+  });
+
+
+  const userType = [
+    {
+      value: 'administrador',
+      label: 'Administrador',
+    },
+    {
+      value: 'departamentoPessoal',
+      label: 'Departamento Pessoal',
+    },
+    {
+      value: 'infraestrutura',
+      label: 'Infraestrutura',
+    },
+    {
+      value: 'marketing',
+      label: 'Marketing',
+    },
+    {
+      value: 'recursosHumanos',
+      label: 'Recursos Humanos',
+    },
+ 
+  ];
+
+
   return (
-
-
     <Container>
       <ModalForm>
         <h2>Editar usuário</h2>
 
-        <form>
-          <TextField/>
-          <TextField/>
-          <TextField/>
-          <Button>Enviar</Button>
-        </form>
+        <GridForm>
+          <TextField
+          variant='outlined'
+          type='text'
+          name='name'
+          id='name'
+          label='Nome do usuário'   
+          onChange={formik.handleChange}    
+          value={formik.values.name}  
+          />
+          <TextField
+               variant='outlined'
+               type='text'
+               name='email'
+               id='email'
+               label='E-mail'       
+               onChange={formik.handleChange}    
+               value={formik.values.email}   
+          />
+          
+          <TextField
+              id="userType"
+              select
+              name="userType"
+              label="Tipo de usuário"
+              variant="outlined"
+              InputLabelProps={{ shrink: true }}
+              value={formik.values.userType}
+              onChange={formik.handleChange}
+          >
+            {userType.map((user) => (
+            <MenuItem key={user.value} value={user.value}>
+              {user.label}
+            </MenuItem>
+            ))}
+          </TextField>
+       
+        </GridForm>
+        <ContainerButtons>
+          <Button 
+          id='CancelButton'>CANCELAR</Button>
+          <Button
+          id='SaveButton'>SALVAR</Button>
+          </ContainerButtons>
       </ModalForm>
 
 
--- src/components/EditUserModal/Style.js
+++ src/components/EditUserModal/Style.js
@@ -1,8 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-
-position: fixed;
+  position: fixed;
   z-index: 1;
   left: 0;
   top: 0;
@@ -12,28 +11,48 @@ position: fixed;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
-export const ModalForm = styled.div`
-    height: 60%;
-    width: 50%;
-    box-sizing: border-box;
-    padding: 25px;
-    background-color: white;
-    border: solid thin #E6E6E6;
-    border-radius: 5px;
-    display: flex;
-    flex-flow: column nowrap;
-    gap: 15px;
+export const ModalForm = styled.form`
+  height: 60%;
+  width: 50%;
+  box-sizing: border-box;
+  padding: 25px;
+  background-color: white;
+  border: solid thin #e6e6e6;
+  border-radius: 5px;
+  display: flex;
+  flex-flow: column nowrap;
+  gap: 25px;
 
-    h2{
-      font-size: 2rem;
-    }
+  h2 {
+    font-size: 2rem;
+  }
+`;
 
-    form{
+export const GridForm = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 25px;
+`;
 
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      gap: 25px;
-    }
-  `
+export const ContainerButtons = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-self: end;
+  align-self: end;
+
+  button {
+    width: 100px;
+    height: 40px;
+  }
+  #CancelButton {
+    border: solid thin #ff5f5d;
+    color: #ff5f5d;
+  }
+
+  #SaveButton {
+    background-color: #0dcd9a;
+    color: white;
+  }
+`;
