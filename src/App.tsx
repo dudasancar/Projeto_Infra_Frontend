a0950552596564a172a6e7ab7c071cbf36f12e85
Douglas Miller Fernandes <Douglas753951@hotmail.com>
2022-04-11 23:19:35 -0300

    Fake Data e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import EmployeesList from "./components/Employees/EmployeesList"
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <EmployeesList/>
+  </div>;
 }
 
 export default App;
---
+++ src/services/Employees/ListEmployees.js
@@ -0,0 +1,60 @@
+
+import api from '../api'
+
+
+export const listEmployees = () => {
+
+const data = [
+    {
+    id: 1,
+    name: "Douglas",
+    email: "douglas.fernandes@gx2.com.br",
+    type:"Desenvolvedor",
+    local: "Frigelar"
+    },
+    {
+    id: 2,
+    name: "Gustavo",
+    email: "gustavo.fernandes@gx2.com.br",
+    type:"Desenvolvedor",
+    local: "Agibank"
+    },
+    {
+    id: 3,
+    name: "Eduarda",
+    email: "eduarda.fernandes@gx2.com.br",
+    type:"Adminsitracao",
+    local: "HomeOffice"
+    },
+    {
+    id: 4,
+    name: "Carlos Eduardo",
+    email: "carlos.fernandes@gx2.com.br",
+    type:"Adminsitracao",
+    local: "NuBank"
+    },
+    {
+    id: 5,
+    name: "Sthefanie",
+    email: "sthe.fernandes@gx2.com.br",
+    type:"Recursos Humanos",
+    local: "GX2"
+    },
+    {
+    id: 6,
+    name: "Roger",
+    email: "roger.fernandes@gx2.com.br",
+    type:"Desenvolvedor",
+    local: "HomeOffice"
+    },
+    
+]
+
+    try{
+        return Promise.resolve(data)
+
+    }catch(error){
+       return Promise.reject(error)
+    }  
+    
+}
