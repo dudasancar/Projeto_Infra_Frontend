a856a25c3d1ba8fa829466ce865a6a99eee92f8c
Gustavo Kunde <gustavokunde@gmail.com>
2022-04-25 17:20:38 -0300

    correção de nomes de arquivos

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,25 +1,24 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../styles/GlobalStyle";
-import AddEditEmployee from "../pages/AddEditEmployee/Index";
+import AddEditEmployee from "../pages/AddEditEmployee";
 import EmployeesList from "../pages/EmployeesList/index";
 import EquipmentsList from "../pages/EquipmentsList/index";
-import Login from "../pages/Login/Index";
-import ModalMessage from "../components/ModalHelper/Index";
+import Login from "../pages/Login";
+import ModalMessage from "../components/ModalHelper";
 import { useMessage } from "../context/MessageContext";
-import MenuNavigation from "../components/MenuNavigation/Index";
-import { useUser } from '../context/UserContext/index';
-import NoAccessHelper from '../components/NoAccessHelper';
+import MenuNavigation from "../components/MenuNavigation";
+import { useUser } from "../context/UserContext/index";
+import NoAccessHelper from "../components/NoAccessHelper";
 
 const MainRoutes = () => {
-
   const { user } = useUser();
   const { message } = useMessage();
 
-  function IdPrivateRoute({ children }){
+  function IdPrivateRoute({ children }) {
     return user.token ? (
       <MenuNavigation> {children} </MenuNavigation>
     ) : (
-     <NoAccessHelper/>
+      <NoAccessHelper />
     );
   }
 
