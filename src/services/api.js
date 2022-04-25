06f1f6e6884ad8a982bc2f42cb48fa8757eb7cf0
Mateus Lammel <lammel.mateus@gmail.com>
2022-04-25 09:33:36 -0300

    Interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-
   baseURL: "http://localhost:3333",
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const user = localStorage.getItem("user") || "{}";
+    if (user != null)
+      config.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
