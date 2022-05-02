d76936cda8f26b33bca9eedc40087fea6dafda1f
Mateus Lammel <lammel.mateus@gmail.com>
2022-05-02 10:48:06 -0300

    Criação do ícone na lista e das pastas

---
+++ src/pages/ListAndLinkEquipmentToCollaborator/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListAndLinkEquipmentToCollaborator = () => {
+  return (
+    <div>ListAndLinkEquipmentToCollaborator</div>
+  )
+}
+
+export default ListAndLinkEquipmentToCollaborator
