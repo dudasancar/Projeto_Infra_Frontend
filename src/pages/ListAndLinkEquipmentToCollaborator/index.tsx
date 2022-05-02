55ad0803acaf799c19ce840a74a7bfd1d6f1a40f
Mateus Lammel <lammel.mateus@gmail.com>
2022-05-02 16:12:53 -0300

    Tabela inicial

--- src/pages/ListAndLinkEquipmentToCollaborator/index.tsx
+++ src/pages/ListAndLinkEquipmentToCollaborator/index.tsx
@@ -1,10 +1,23 @@
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Tooltip } from "@mui/material";
+import MaterialTable from "material-table";
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useMessage } from "../../context/MessageContext";
 import { getCollaborator } from "../../services/Collaborators/getCollaborator";
-import { listCollaborators } from "../../services/Collaborators/listCollaborators";
 import { AddEquipmentContainer, Container } from "./styles";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+interface IEquipment {
+  id: string;
+  name: string;
+  serial_number: string;
+  model: string;
+  type: string;
+  stituation: string;
+  status: string;
+  created_at: Date;
+  collaborator: {};
+}
 
 const ListAndLinkEquipmentToCollaborator = () => {
   const { id } = useParams();
@@ -14,7 +27,7 @@ const ListAndLinkEquipmentToCollaborator = () => {
   useEffect(() => {
     getCollaborator(id)
       .then((response: any) => {
-        setEquipments(response.data.equipments)
+        setEquipments(response.data.equipments);
       })
       .catch((err) =>
         setMessage({
@@ -34,6 +47,45 @@ const ListAndLinkEquipmentToCollaborator = () => {
           <Button></Button>
         </form>
       </AddEquipmentContainer>
+
+      {equipments && (
+        <MaterialTable
+          data={equipments}
+          title="Lista de Equipmanetos"
+          columns={[
+            { title: "Nome", field: "name" },
+            { title: "Modelo", field: "model" },
+            { title: "Tipo", field: "type" },
+            {
+              title: "",
+              render: (equipment: IEquipment) => (
+                <Tooltip title="Inativar">
+                  <DeleteForeverIcon
+                   
+                    style={{
+                      cursor: "pointer",
+                      color: "red",
+                    }}
+                  />
+                </Tooltip>
+              ),
+            },
+          ]}
+          options={{
+            filtering: true,
+            search: true,
+            paging: true,
+            headerStyle: {
+              backgroundColor: "#DFDFDF",
+              color: "#334B48",
+              fontSize: "0.9rem",
+              fontStyle: "italic",
+              paddingBottom: "1px",
+              paddingTop: "1px",
+            },
+          }}
+        />
+      )}
     </Container>
   );
 };
