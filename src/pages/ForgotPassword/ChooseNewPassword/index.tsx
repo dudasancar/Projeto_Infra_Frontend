dde4cc1a3400cd6545e99a59bb577908861e70a9
Mateus Lammel <lammel.mateus@gmail.com>
2022-05-03 14:24:52 -0300

    Várias modificações para a requisição de troca de senha

--- src/pages/ForgotPassword/ChooseNewPassword/index.tsx
+++ src/pages/ForgotPassword/ChooseNewPassword/index.tsx
@@ -1,22 +1,57 @@
 import { Button, TextField } from "@mui/material";
 import { useFormik } from "formik";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { object, string } from "yup";
 import { useMessage } from "../../../context/MessageContext";
 import { ChangeEmployeePassword } from "../../../services/Employees/ForgotPassword/forgotPassword";
 import { Container, ContainerForm } from "./styles";
+import jwt_decode from "jwt-decode";
+import { getEmployee } from "../../../services/Employees/getEmployee";
+
+interface IToken {
+  exp: number;
+  iat: number;
+  id: string;
+}
+
+interface IEmployee {
+  name: string;
+  id: string;
+  password: string;
+  type: string;
+  status: string;
+  created_at: Date;
+}
 
 const ChooseNewPassword = () => {
   const { setMessage } = useMessage();
   const { token } = useParams();
   const navigate = useNavigate();
+  const decryptToken = jwt_decode<IToken>(token!);
+  const [employee, setEmployee] = React.useState<IEmployee>();
+
+  useEffect(() => {
+    getEmployee(decryptToken.id)
+      .then((response: any) => {
+        setEmployee(response.data);
+      })
+      .catch((err) =>
+        setMessage({
+          content: `O seguinte erro ocorreu ao buscar os dados do usuário: ${err}`,
+          display: true,
+          severity: "error",
+        })
+      );
+  }, []);
 
   const setNewPassword = (values: {
-    password: string;
-    confirmPassword: string;
+    id: string;
+    oldPassword: string;
+    newPassword: string;
+    confirmNewPassword: string;
   }) => {
-    ChangeEmployeePassword(values, token)
+    ChangeEmployeePassword(values)
       .then(() => {
         setMessage({
           content: "Senha alterada com sucesso",
@@ -35,21 +70,30 @@ const ChooseNewPassword = () => {
   };
 
   const validationSchema = object({
-    password: string()
+    newPassword: string()
       .min(6, "A senha deve possuír no mínimo 6 caracteres")
       .required("Senha obrigatória"),
-    confirmPassword: string()
+   confirmNewPassword: string()
       .min(6, "A senha deve possuír no mínimo 6 caracteres")
       .required("Confirmação da senha obrigatória"),
   });
 
   const formik = useFormik({
     initialValues: {
-      password: "",
-      confirmPassword: "",
+      oldPassword: "",
+      newPassword: "",
+      confirmNewPassword: "",
+      id: "",
     },
     validationSchema: validationSchema,
-    onSubmit: (values: { password: string; confirmPassword: string }) => {
+    onSubmit: (values: {
+      id: string;
+      oldPassword: string;
+      newPassword: string;
+     confirmNewPassword: string;
+    }) => {
+      values.oldPassword = employee!.password;
+      values.id = decryptToken.id;
       setNewPassword(values);
     },
   });
@@ -63,28 +107,28 @@ const ChooseNewPassword = () => {
           <TextField
             variant="outlined"
             type="password"
-            name="password"
-            id="password"
+            name="newPassword"
+            id="newPassword"
             label="Nova senha"
             onChange={formik.handleChange}
-            value={formik.values.password}
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
+            value={formik.values.newPassword}
+            error={formik.touched.newPassword && Boolean(formik.errors.newPassword)}
+            helperText={formik.touched.newPassword && formik.errors.newPassword}
           />
           <TextField
             variant="outlined"
             type="password"
-            name="confirmPassword"
-            id="confirmPassword"
+            name="confirmNewPassword"
+            id="confirmNewPassword"
             label="Confirmar nova senha"
             onChange={formik.handleChange}
-            value={formik.values.confirmPassword}
+            value={formik.values.confirmNewPassword}
             error={
-              formik.touched.confirmPassword &&
-              Boolean(formik.errors.confirmPassword)
+              formik.touched.confirmNewPassword &&
+              Boolean(formik.errors.confirmNewPassword)
             }
             helperText={
-              formik.touched.confirmPassword && formik.errors.confirmPassword
+              formik.touched.confirmNewPassword && formik.errors.confirmNewPassword
             }
           />
           <Button type="submit" variant="contained" size="large">
