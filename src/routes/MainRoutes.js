1c866f92b164650d260fb24a5558ed0502f1cd90
rogerxavier29 <85241974+eezsm93@users.noreply.github.com>
2022-05-12 14:12:04 -0300

    tirando importes nao utilizados

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -2,26 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "../styles/GlobalStyle";
 import ModalMessage from "../components/ModalHelper";
 import { useMessage } from "../context/MessageContext";
-import MenuNavigation from "../components/MenuNavigation";
-import { useUser } from "../context/UserContext/index";
-import NoAccessHelper from "../components/NoAccessHelper";
 import PublicRoutes from "./PublicRoutes/PublicRoutes";
 import PrivateRoutes from "./PrivateRoutes/PrivateRoutes";
 
 const MainRoutes = () => {
- 
   const { message } = useMessage();
-
- 
 
   return (
     <BrowserRouter>
       {message.display && <ModalMessage />}
       <GlobalStyle />
       <PublicRoutes />
-    
-        <PrivateRoutes />
-     
+
+      <PrivateRoutes />
     </BrowserRouter>
   );
 };
